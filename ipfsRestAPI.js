d1fa39b5d750a282d8b5643cea1636d968870366
Edward Boggis-Rolfe <edward@boggis-rolfe.com>
2019-11-30 13:01:09 +0000

    some extra expressy stuff

--- ipfsRestAPI.js
+++ ipfsRestAPI.js
@@ -22,6 +22,8 @@ app.get("/api/ping", function(req, res){
   res.json({ messaage: "pong" });
 });
 
+app.use(express.static(path.join(__dirname, 'html')));
+
 //app.use((req, res) => {
 //	res.send('Hello there !');
 //});
